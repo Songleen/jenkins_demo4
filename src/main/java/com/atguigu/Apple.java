f74eb2e70e53b5a30f5101c13396a408899d0a08
sellee7092@163.com <sellee7092@163.com>
2019-10-16 17:00:12 +0800

    第九次提交

--- src/main/java/com/atguigu/Apple.java
+++ src/main/java/com/atguigu/Apple.java
@@ -7,7 +7,7 @@ package com.atguigu;
  */
 public class Apple {
 
-    private String appName = "this are five bad apples";
+    private String appName = "this are SIX bad apples";
 
     public String getAppName() {
         return appName;
