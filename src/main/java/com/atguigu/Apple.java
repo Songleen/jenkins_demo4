c99aa68c5c5f028720308309a8041c6149776fdd
sellee7092@163.com <sellee7092@163.com>
2019-10-16 09:27:04 +0800

    初次提交

---
+++ src/main/java/com/atguigu/Apple.java
@@ -0,0 +1,15 @@
+package com.atguigu;
+
+/**
+ * @ClassName jenkins_demo3
+ * @Author Songleen
+ * @Date 2019/10/09/14:54
+ */
+public class Apple {
+
+    private String appName = "this is a big apple";
+
+    public String getAppName() {
+        return appName;
+    }
+}
